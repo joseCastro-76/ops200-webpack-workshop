77066c5c8062b216601d9c58494478c7b7d88c05
joseCastro-76 <56211699+joseCastro-76@users.noreply.github.com>
2020-08-05 11:33:31 -0700

    initial upload.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from ' react-dom';
+
+const rootElement = document.getElementById('root');
+const sampleComponent = 
+React and webpack are working!
+;
+
+ReactDOM.render(sampleComponent, rootElement);
